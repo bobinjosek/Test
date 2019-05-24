0907aea7af820ce4a0e93a25214028e7b87bf834
bobinjosek <bobinjosek@gmail.com>
2019-05-24 11:44:03 +0530

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-touch /home/bobin
+FROM registry.redhat.io/jboss-webserver-3/webserver30-tomcat8-openshift:1.2
+RUN sed -i "s|shared.loader=|shared.loader=/mnt/config,/newrelic/|" /opt/webserver/conf/catalina.properties
